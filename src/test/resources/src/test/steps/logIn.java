40b2345daf0eaafbe72e8f5bce7c2b4b627c4d2b
Maninder Dhanju <singhmaninder21@gmail.com>
2015-03-08 00:11:01 -0800

    adding steps

--- src/test/resources/src/test/steps/logIn.java
+++ src/test/resources/src/test/steps/logIn.java
@@ -8,42 +8,53 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class logIn {
-	private browser connector;
+	 private browser connector;
 
-	public logIn(browser connector) {
-		this.connector = connector;
+	  public logIn(browser connector) {
+	    this.connector = connector;
+	  }
+
+	  @Before
+	  public void beforeScenario() {
+	    System.out.println(" *** Before");
+	  }
+
+	  @After
+	  public void afterScenario() {
+	    System.out.println(" *** After");
+	  }
+	  
+	  @Given("^I am a log In screen for Gmail$")
+	  public void i_am_a_log_In_screen_for_Gmail() throws Throwable {
+	      // Write code here that turns the phrase above into concrete actions
+		  connector.openAndWait("https://www.gmail.com/");
+		  
+	  }
+
+	  @Then("^I should see input for \"(.*?)\"$")
+	  public void i_should_see_input_for(String arg1) throws Throwable {
+	      // Write code here that turns the phrase above into concrete actions
+		  assertTrue(true);
+	  }
+
+	  @Then("^I should see Sign In button$")
+	  public void i_should_see_Sign_In_button() throws Throwable {
+	      // Write code here that turns the phrase above into concrete actions
+		  assertTrue(true);
+	  }
+
+	  @Given("^I am a log In screen for Gmail with \"(.*?)\"$")
+	  public void i_am_a_log_In_screen_for_Gmail_with(String arg1) throws Throwable {
+	      // Write code here that turns the phrase above into concrete actions
+		  assertTrue(true);
+	  }
+
+	  @Then("^I should see link \"(.*?)\"$")
+	  public void i_should_see_link(String arg1) throws Throwable {
+	      // Write code here that turns the phrase above into concrete actions
+		  assertTrue(true);
+	  }
+
+
+
 	}
-
-	@Given("^I am a log In screen for Gmail$")
-	public void i_am_a_log_In_screen_for_Gmail() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		connector.openAndWait("www.google.com");
-
-	}
-
-	@Then("^I should see input for \"(.*?)\"$")
-	public void i_should_see_input_for(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		// assertTrue(true);
-	}
-
-	@Then("^I should see Sign In button$")
-	public void i_should_see_Sign_In_button() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		// assertTrue(true);
-	}
-
-	@Given("^I am a log In screen for Gmail with \"(.*?)\"$")
-	public void i_am_a_log_In_screen_for_Gmail_with(String arg1)
-			throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		// assertTrue(true);
-	}
-
-	@Then("^I should see link \"(.*?)\"$")
-	public void i_should_see_link(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		// assertTrue(true);
-	}
-
-}
